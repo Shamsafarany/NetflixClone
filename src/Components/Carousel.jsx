acc9992813e04131536c6e03ab9b1eb7c0f1194a
Sham Safarany <125689014+Shamsafarany@users.noreply.github.com>
2025-09-11 13:01:56 +0300

    fix carousel

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,12 +1,42 @@
 import "../styles/carousel.css";
 import Slide from "./Slide.jsx";
+import { useState, useEffect } from "react";
 
 function Carousel({ movies }) {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevSlide = () => {
+    setCurrentIndex((prev) => (prev === 0 ? movies.length - 1 : prev - 1));
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prev) => (prev === movies.length - 1 ? 0 : prev + 1));
+  };
+
+  useEffect(() => { 
+    const interval = setInterval(() =>{
+      nextSlide();
+    }, 5000);
+    return () =>{
+      clearInterval(interval);
+    }
+  }, [currentIndex]);
+
   return (
     <div className="carouselCont">
-      {movies.map((movie, index) => {
-        return <Slide key={index} movie={movie} />;
-      })}
+      <button onClick={prevSlide} className="carouselBtn left">
+        &#10094;
+      </button>
+      <div className="slidesWrapper">
+        {movies.map((movie, index) => (
+          <Slide key={index} movie={movie} active={index === currentIndex} />
+        ))}
+      </div>
+
+      <button onClick={nextSlide} className="carouselBtn right">
+        &#10095;
+      </button>
     </div>
   );
 }
--- src/Components/Slide.jsx
+++ src/Components/Slide.jsx
@@ -1,9 +1,10 @@
 import img from "../assets/stranger.jpeg";
 import "../Styles/carousel.css";
 import logo from "../assets/netflix_732228.png";
-function Slide({ movie }) {
+import playBtn from "../assets/icons8-play-50.png";
+function Slide({ movie , active }) {
   return (
-    <div className="slide">
+    <div className={`slide ${active ? "active" : ""}`}>
       <img src={img} alt={movie.title} className="slideImg" />
       <div className="slideInfo">
         <div className="text">
@@ -11,12 +12,16 @@ function Slide({ movie }) {
             <img src={logo} alt="Netflix Logo" className="logoSlide" />
             <span className="series">SERIES</span>
           </div>
-          <h3>
-            {movie.title}
-          </h3>
-          <p> <span>{movie.year}</span> <br />
-            Info about show</p>
-          <button>Play</button>
+          <h3>{movie.title}</h3>
+          <p>
+            {" "}
+            <span>{movie.year}</span> <br />
+            Info about show
+          </p>
+          <button className="play">
+            <img src={playBtn} alt="playBtn" />
+            Play
+          </button>
         </div>
       </div>
     </div>
