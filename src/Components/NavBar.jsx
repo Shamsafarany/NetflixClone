e922e8e7bd2a5945379fcf24e0c633a26f6043a7
Sham Safarany <125689014+Shamsafarany@users.noreply.github.com>
2025-09-15 15:50:27 +0300

    fix scroll navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,40 +1,66 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import img from "../assets/icons8-netflix (1).svg";
 import "../styles/navBar.css";
 import search from "../assets/icons8-search (2).svg";
 import send from "../assets/icons8-send-48.png";
 import closeIcon from "../assets/icons8-close-50.png";
-import {Link} from "react-router-dom";
+import Grid from "../Components/Grid";
+import { searchMovies } from "../Services/api";
+import { Link } from "react-router-dom";
 
 function NavBar({ type = "default" }) {
   const [showInput, setShowInput] = useState(false);
-  const [showSearch, setShowSearch] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   function handleInput(e) {
     if (e.target.value === "") {
       return;
     }
-    setShowSearch(e.target.value.trim());
+    setSearchQuery(e.target.value.trim());
   }
 
   function handleShowInput() {
     setShowInput(true);
   }
-  function handleSubmit(e) {
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    if (showSearch === "") {
+    if (!searchQuery.trim()) {
       return;
     }
-    setShowSearch("");
-    setShowInput(false);
+    try {
+      const results = await searchMovies(searchQuery);
+      setMovies(results);
+    } catch (error) {
+      console.log(error);
+    }
+    setSearchQuery("");
   }
   function handleClearInput() {
-    setShowSearch("");
+    setSearchQuery("");
     setShowInput(false);
   }
 
   return (
     <>
-      <nav className="navBar">
+      <nav className={`navBar ${scrolled ? "scrolled" : ""}`}>
         {type === "default" ? (
           <>
             <div className="cont1">
@@ -69,27 +95,42 @@ function NavBar({ type = "default" }) {
             </div>
             <div className="cont2">
               {showInput ? (
-                <form className="searchForm" onSubmit={handleSubmit}>
-                  <div className="wrapper">
-                    <input
-                      type="text"
-                      placeholder="Search for shows..."
-                      onChange={handleInput}
-                      value={showSearch}
-                      autoFocus
-                    />
-                    <img
-                      src={closeIcon}
-                      alt="close"
-                      className="closeIcon"
-                      onClick={handleClearInput}
-                    />
-                  </div>
+                <>
+                  <form className="searchForm">
+                    <div className="wrapper">
+                      <input
+                        type="text"
+                        placeholder="Search for shows..."
+                        onChange={handleInput}
+                        value={searchQuery}
+                        autoFocus
+                      />
+                      <img
+                        src={closeIcon}
+                        alt="close"
+                        className="closeIcon"
+                        onClick={handleClearInput}
+                      />
+                    </div>
 
-                  <button type="submit" className="sendBtn">
-                    <img src={send} alt="search" className="send" />
-                  </button>
-                </form>
+                    <button
+                      type="submit"
+                      className="sendBtn"
+                      onClick={handleSubmit}
+                    >
+                      <img src={send} alt="search" className="send" />
+                    </button>
+                  </form>
+                  <div className="searchResults">
+                    {movies.length > 0 && (
+                      <Grid
+                        movies={movies}
+                        type="movies"
+                        className="searchResults"
+                      />
+                    )}
+                  </div>
+                </>
               ) : (
                 <img
                   src={search}
